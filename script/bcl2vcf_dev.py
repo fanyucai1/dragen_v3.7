7cdd0f0789566590fdcf777eb2623a62ddefe4b7
fanyucai1 <fanyucai1@126.com>
2021-01-07 14:33:25 +0800

    no

--- script/bcl2vcf_dev.py
+++ script/bcl2vcf_dev.py
@@ -17,6 +17,7 @@ parser.add_argument("-g","--wgs",help="wgs sample list")
 parser.add_argument("-o","--outdir",help="output directory")
 parser.add_argument("-ne","--normal_wes",help="exon normal list file",default="false")
 parser.add_argument("-ng","--normal_wgs",help="wgs normal list file",default="false")
+parser.add_argument("-c","--config",help="tgex config file",default="false")
 args=parser.parse_args()
 ##########################################################check once again
 python3 = subprocess.getoutput('which python3')
@@ -47,51 +48,21 @@ core.bcl2fastq.run(args.samplesheet,args.indir,fastq_dir)
 #########################################################parse sample list and mkdir wes/wgs vcf output directory
 fastq2vcf_wes_par,fastq2vcf_wgs_par,wes_vcf,wgs_vcf={},{},"",""
 if args.wes:
-    fastq2vcf_wes_par=core.parse_samplelist.run(args.wes)
+    fastq2vcf_wes_par=core.run_tgex.run(args.wes)
     wes_vcf = out_path + "/wes_vcf"
     subprocess.check_call("mkdir -p %s" % (wes_vcf), shell=True)
     core.wes.run(args.ref, args.wes, fastq_dir, wes_vcf,args.bed,args.normal_wes)#fastq2vcf
     core.result_parse.run(wes_vcf, "%s.wes" % (localtime), out_path)  # output total matrix
     core.copy2vcf.run(wes_vcf, "%s/combine_wes_vcf/" % (out_path))  # copy pass vcf to one directoy
+    if args.cofig!="false":
+        core.run_tgex.run(args.wes,args.config,"%s/combine_wes_vcf/" % (out_path))
 if args.wgs:
     wgs_vcf = out_path + "/wgs_vcf"
     subprocess.check_call("mkdir -p %s" % (wgs_vcf), shell=True)
     core.wgs.run(args.ref,args.wgs,fastq_dir,wgs_vcf,args.normal_wgs)
     core.result_parse.run(wgs_vcf, "%s.wgs" % (localtime), out_path)  # output total matrix
     core.copy2vcf.run(wgs_vcf, "%s/combine_wgs_vcf/" % (out_path))  # copy pass vcf to one directoy
-    fastq2vcf_wgs_par=core.parse_samplelist.run(args.wgs)
-#########################################################
-if args.wes:
-    ##############################################################maybe run tgex
-    for sample_name in fastq2vcf_wes_par:
-        if fastq2vcf_wes_par[sample_name]!="false":
-            tgex_script = "%s %s/Tgex/tgex_uploadSamples.py --config %s/Tgex/tgex.config.yml %s " % (python3, dir_name, dir_name,fastq2vcf_wes_par[sample_name])
-            for (root,dirs,files) in os.walk("%s/combine_wes_vcf/"%(out_path)):
-                for file in files:
-                    if re.search(sample_name,file):
-                        if re.search('.sv.pass.vcf.gz$',file):
-                            tgex_script+=" --svVcf %s "%(os.path.join(root,file))
-                        if re.search('.hard-filtered.pass.vcf.gz$',file):
-                            tgex_script+=" --snvVcf %s "%(os.path.join(root,file))
-                        if re.search('.cnv.pass.vcf.gz$',file):
-                            tgex_script+=" --cnvVcf %s "%(os.path.join(root,file))
-            print(tgex_script)
-            subprocess.check_call(tgex_script, shell=True)
-if args.wgs:
-    #########################################################maybe run tgex
-    for sample_name in fastq2vcf_wgs_par:
-        if fastq2vcf_wgs_par[sample_name] != "false":
-            tgex_script = "%s %s/Tgex/tgex_uploadSamples.py --config %s/Tgex/tgex.config.yml %s " % (python3, dir_name, dir_name,fastq2vcf_wgs_par[sample_name])
-            for (root, dirs, files) in os.walk("%s/combine_wgs_vcf/" % (out_path)):
-                for file in files:
-                    if re.search(sample_name, file):
-                        if re.search('.sv.pass.vcf.gz$', file):
-                            tgex_script += " --svVcf %s " % (os.path.join(root, file))
-                        if re.search('.hard-filtered.pass.vcf.gz$', file):
-                            tgex_script += " --snvVcf %s " % (os.path.join(root, file))
-                        if re.search('.cnv.pass.vcf.gz$', file):
-                            tgex_script += " --cnvVcf %s " % (os.path.join(root, file))
-            print(tgex_script)
-            subprocess.check_call(tgex_script,shell=True)
+    if args.cofig != "false":
+        core.run_tgex.run(args.wgs, args.config, "%s/combine_wgs_vcf/" % (out_path))
 #########################################################
 print("Done")
---
+++ script/core/run_tgex.py
@@ -0,0 +1,49 @@
+import re,sys,os,subprocess
+sub=os.path.abspath(__file__)
+dir_name=os.path.dirname(sub)
+sys.path.append(dir_name)
+
+def run(samplelist,config_file,indir):
+    ################################
+    infile=open(samplelist,"r")
+    par_name,num,sample_par=[],0,{}
+    for line in infile:
+        num+=1
+        array=line.strip().split(",")
+        if len(array)>1:
+            if num == 1:
+                if len(array) > 1:
+                    for i in range(0, len(array)):
+                        par_name.append(array[i])  # 获得参数名
+            else:
+                sample_par[array[0]] = ""
+                if len(array) > 1:
+                    for i in range(1, len(array)):
+                        sample_par[array[0]] += "--" + par_name[i] + " "
+                        sample_par[array[0]] += array[i] + " "
+                else:
+                    sample_par[array[0]] = "false"
+        else:
+            exit()
+    infile.close()
+    for (root, dirs, files) in os.walk(indir):
+        for sample_name in sample_par:
+            tgex_script = "python3 %s/tgex_uploadSamples.py --config %s " % (dir_name, config_file)
+            for file in files:
+                if re.search(sample_name, file):
+                    if re.search('.sv.pass.vcf.gz$', file):
+                        tgex_script += " --svVcf %s " % (os.path.join(root, file))
+                    if re.search('.hard-filtered.pass.vcf.gz$', file):
+                        tgex_script += " --snvVcf %s " % (os.path.join(root, file))
+                    if re.search('.cnv.pass.vcf.gz$', file):
+                        tgex_script += " --cnvVcf %s " % (os.path.join(root, file))
+            print(tgex_script)
+            subprocess.check_call(tgex_script, shell=True)
+
+
+if __name__=="__main__":
+    if len(sys.argv)!=4:
+        print("usage:\npython3 %s sample.list /path/to/tgex.config.yml /indir_vcf/\n"%(sys.argv[0]))
+        print("\n#Email:yucai.fan@illumina.com\n")
+    else:
+        run(sys.argv[1],sys.argv[2],sys.argv[3])
