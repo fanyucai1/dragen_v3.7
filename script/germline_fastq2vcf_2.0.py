f0fb0e17e70e392f0ee1ded3ffa9012586ba4411
fanyucai1 <fanyucai1@126.com>
2021-01-21 20:33:58 +0800

    no

--- script/germline_fastq2vcf_2.0.py
+++ script/germline_fastq2vcf_2.0.py
@@ -46,6 +46,6 @@ for i in range(0,len(samplename)):
                     cmd += " -1 %s -2 %s " % (R1, R2)
                     if not os.path.exists("%s"% (args.out_dir+"/"+samplename[i])):
                         subprocess.check_call("mkdir -p %s"% (args.out_dir+"/"+i),shell=True)
-                        subprocess.check_call(cmd,shell=True)
+                        #subprocess.check_call(cmd,shell=True)
                         shell.write(cmd)
 shell.close()
