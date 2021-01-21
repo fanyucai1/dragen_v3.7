10d0ee7a95024b39b65895088cc1ee752d0b8400
fanyucai1 <fanyucai1@126.com>
2021-01-21 20:28:23 +0800

    no

--- script/germline_fastq2vcf_2.0.py
+++ script/germline_fastq2vcf_2.0.py
@@ -27,18 +27,22 @@ for i in range(0,len(samplename)):
         cmd += " --enable-duplicate-marking true --output-format BAM --enable-map-align true "
         cmd += " --enable-map-align-output true --enable-bam-indexing true --enable-variant-caller true "
         cmd += " --enable-cnv true --enable-sv true "
+        cmd += " --RGID Illumina_RGID --RGSM %s " % (samplename[i])
+        cmd += " --output-directory %s --output-file-prefix %s " % (args.out_dir + "/" + samplename[i], samplename[i])
         if args.PoN != "false":
-            cmd += " --cnv-normals-list %s --cnv-enable-self-normalization false --cnv-enable-gcbias-correction false " % (
-                args.PoN)
+            cmd += " --cnv-normals-list %s --cnv-enable-self-normalization false --cnv-enable-gcbias-correction false " % (args.PoN)
         else:
             cmd += " --cnv-enable-self-normalization true --cnv-enable-gcbias-correction true "
         if args.bed != "false":
-            cmd += " --sv-exome true --vc-target-bed %s --cnv-target-bed %s --sv-call-regions-bed %s --cnv-interval-width 1000 " % (
-            args.bed, args.bed, args.bed)
+            cmd += " --sv-exome true --vc-target-bed %s --cnv-target-bed %s --sv-call-regions-bed %s --cnv-interval-width 1000 " % (args.bed, args.bed, args.bed)
         else:
             cmd += " --repeat-genotype-enable=true --repeat-genotype-specs=/opt/edico/repeat-specs/hg19/ "
-        cmd += " --RGID Illumina_RGID --RGSM %s " % (samplename[i])
-        cmd += " --output-directory %s --output-file-prefix %s " % (args.out_dir + "/" + samplename[i], samplename[i])
-        if not os.path.exists("%s"% (args.out_dir+"/"+samplename[i])):
-            subprocess.check_call("mkdir -p %s"% (args.out_dir+"/"+i),shell=True)
-            subprocess.check_call(cmd,shell=True)
+        for (root, dirs, files) in os.walk(args.fastq_dir):
+            for file in files:
+                if re.search('fastq.gz$', file) and re.search("_R1_", file) and re.search("%s_S"%(samplename[i]), file):
+                    R1 = os.path.join(root, file)
+                    R2 = R1.replace("_R1_", "_R2_")
+                    cmd += " -1 %s -2 %s " % (R1, R2)
+                    if not os.path.exists("%s"% (args.out_dir+"/"+samplename[i])):
+                        subprocess.check_call("mkdir -p %s"% (args.out_dir+"/"+i),shell=True)
+                        subprocess.check_call(cmd,shell=True)
