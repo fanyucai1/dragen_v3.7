47bd2d8705bffca711549d32ba1759b64928c280
fanyucai1 <fanyucai1@126.com>
2021-01-21 20:22:02 +0800

    no

---
+++ script/germline_fastq2vcf_2.0.py
@@ -0,0 +1,44 @@
+import sys
+import subprocess
+import os
+import re
+import argparse
+
+parse=argparse.ArgumentParser("Run germline pipeline from fastq2vcf.")
+parse.add_argument("-r","--ref",help="reference directory",required=True)
+parse.add_argument("-s","--samplelist",help="sample list",required=True)
+parse.add_argument("-o","--out_dir",help="output directory",required=True)
+parse.add_argument("-b","--bed",help="bed file",default="false")
+parse.add_argument("-p","--PoN",help="PoN list",default="false")
+parse.add_argument("-f","--fastq_dir",help="fastq directory",required=True)
+args=parse.parse_args()
+
+infile=open(args.samplelist,"r",encoding='gbk')
+samplename=[]
+for line in infile:
+    samplename.append(line.strip().split(",")[0])
+infile.close()
+
+for i in range(0,len(samplename)):
+    if os.path.exists("%s.time_metrics.csv" % (args.out_dir + "/" + samplename[i] + "/" + samplename[i])):
+        exit()
+    else:
+        cmd = "dragen -f -r %s " % (args.ref)
+        cmd += " --enable-duplicate-marking true --output-format BAM --enable-map-align true "
+        cmd += " --enable-map-align-output true --enable-bam-indexing true --enable-variant-caller true "
+        cmd += " --enable-cnv true --enable-sv true "
+        if args.PoN != "false":
+            cmd += " --cnv-normals-list %s --cnv-enable-self-normalization false --cnv-enable-gcbias-correction false " % (
+                args.PoN)
+        else:
+            cmd += " --cnv-enable-self-normalization true --cnv-enable-gcbias-correction true "
+        if args.bed != "false":
+            cmd += " --sv-exome true --vc-target-bed %s --cnv-target-bed %s --sv-call-regions-bed %s --cnv-interval-width 1000 " % (
+            args.bed, args.bed, args.bed)
+        else:
+            cmd += " --repeat-genotype-enable=true --repeat-genotype-specs=/opt/edico/repeat-specs/hg19/ "
+        cmd += " --RGID Illumina_RGID --RGSM %s " % (samplename[i])
+        cmd += " --output-directory %s --output-file-prefix %s " % (args.out_dir + "/" + samplename[i], samplename[i])
+        if not os.path.exists("%s"% (args.out_dir+"/"+samplename[i])):
+            subprocess.check_call("mkdir -p %s"% (args.out_dir+"/"+i),shell=True)
+            subprocess.check_call(cmd,shell=True)
