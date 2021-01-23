06eb8a96831cf0cce6c58f9516bb352afccba460
fanyucai1 <fanyucai1@126.com>
2021-01-23 12:25:08 +0800

    no

--- script/test/test.py
+++ script/test/test.py
@@ -1,3 +1,8 @@
-import mimetypes
-print(mimetypes.guess_type("/Users/fanyucai/Desktop/sample.csv")[0])
-print(mimetypes.guess_type("/Users/fanyucai/Desktop/sample.txt")[0])
+for i in range(1,10):
+    print(i)
+    for j in range(3,10):
+        if j==i:
+            print(j)
+    for m in range(7, 10):
+        if m == i:
+            print(m)
